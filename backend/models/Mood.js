9d03560acc987b0cf21852797386f47d5f160a90
Shaun Wilson <shaunbrianwilsonventures@gmail.com>
2025-12-11 23:38:26 +0000

    Added reference to User in Mood model

--- backend/models/Mood.js
+++ backend/models/Mood.js
@@ -4,6 +4,10 @@ const MoodSchema = new mongoose.Schema({
   emoji: { type: String, required: true },
   note: { type: String },
   date: { type: Date, default: Date.now },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 module.exports = mongoose.model('Mood', MoodSchema);
