54e92bcae29b0dc2525cb7fb630bc07d8f8107cf
Shaun Wilson <shaunbrianwilsonventures@gmail.com>
2025-12-13 21:02:38 +0000

    Wrapped App.jsx in AuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
+import AuthProvider from './context/AuthContext';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
