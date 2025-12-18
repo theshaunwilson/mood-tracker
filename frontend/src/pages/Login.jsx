d890af7ad19cd09032de35c6c100480979dbe9cc
Shaun Wilson <shaunbrianwilsonventures@gmail.com>
2025-12-18 09:26:28 +0000

    Signup form input and labels created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action=""></form>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { useState } from 'react';
+
+// todo - onChange / Value / console.log
 
 function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      console.log(email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          type="email"
+          placeholder="Enter your email..."
+        />
+
+        <label value={password} htmlFor="password">
+          Password
+        </label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+          type="password"
+          placeholder="Enter your password..."
+        />
+        <button>Signup</button>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
