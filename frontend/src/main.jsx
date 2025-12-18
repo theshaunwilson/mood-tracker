42a11c67f934744a60cbd4c912671cf76a2cc1f5
Shaun Wilson <shaunbrianwilsonventures@gmail.com>
2025-12-18 09:36:30 +0000

    Installed tailwincss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './context/AuthContext';
+import './index.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
