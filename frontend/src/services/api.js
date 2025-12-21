b5092e74af4b8de43cd1ebed637e0dcb938cdf95
Shaun Wilson <shaunbrianwilsonventures@gmail.com>
2025-12-21 16:26:10 +0000

    Added Apu interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,12 @@ const API = axios.create({
   baseURL: '/api',
 });
 
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default API;
