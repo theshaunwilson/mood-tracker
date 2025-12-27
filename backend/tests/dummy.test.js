51136193b190f84e31a5f03d8472415483de3490
Shaun Wilson <shaunbrianwilsonventures@gmail.com>
2025-12-27 16:38:39 +0000

    Created list_helper.js with helper functions

---
+++ backend/tests/dummy.test.js
@@ -0,0 +1,21 @@
+const dummy = (moods) => {
+  return 1;
+};
+
+const totalMoods = (moods) => {
+  return moods.reduce((acc, blog) => acc + blog, 0);
+};
+
+const moodCounts = (moods) => {
+  const moodTally = {};
+
+  moods.forEach((mood) => {
+    moodTally[mood.emoji] = moodTally[mood.emoji]
+      ? moodTally[mood.emoji] + 1
+      : 1;
+  });
+
+  return moodTally;
+};
+
+module.exports(dummy, totalMoods, moodCounts);
