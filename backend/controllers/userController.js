9d62bd9b16a81337aefd55f42b9790618b4321b4
Shaun Wilson <shaunbrianwilsonventures@gmail.com>
2026-01-01 21:06:41 +0000

    Created a userController with getUser that finds user, removed id and populates moods with all fields

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/User');
+const Mood = require('../models/Mood');
+
+exports.getUser = async (req, res) => {
+  try {
+    const users = await User.find({})
+      .select('-password')
+      .populate('moods', { emoji: 1, note: 1, date: 1 });
+    res.json(users);
+  } catch (error) {
+    console.error('getMood error', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,12 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  moods: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Mood',
+    },
+  ],
 });
 
 UserSchema.pre('save', async function () {
